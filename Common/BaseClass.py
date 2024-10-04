01a9154c21437a4c2d862b9253e5e7d18c6a5c32
simeonzografov <simeon.zografov@hedgehoglab.com>
2024-10-04 11:08:00 +0300

    Add runner drivers

--- Common/BaseClass.py
+++ Common/BaseClass.py
@@ -1,13 +1,15 @@
 import os
 import pytest
 import allure
-import shutil
-import subprocess
 from dotenv import load_dotenv
 from pytest_check import check
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.edge.service import Service as EdgeService
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
 
 
 def check_with_screenshot(driver, cond, message):
@@ -35,14 +37,41 @@ class BaseClass:
         browser = request.param
 
         project_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        if browser == "Edge":
-            edge_driver_path = os.path.join(project_folder, 'Resources', 'msedgedriver')
-            serv = EdgeService(edge_driver_path)
-            driver = webdriver.Edge(service=serv)
+
+        is_ci = os.getenv('CI') == 'true'
+        if is_ci:
+            if browser == "Edge":
+                # Set up Edge options
+                options = EdgeOptions()
+                options.add_argument("--headless")
+                options.add_argument("--disable-gpu")
+                options.add_argument("--window-size=1920,1080")
+                options.add_argument("--no-sandbox")
+                options.add_argument("--disable-dev-shm-usage")
+                options.add_argument("--disable-extensions")
+                options.add_argument("--disable-infobars")
+                serv = EdgeService(EdgeChromiumDriverManager().install())
+                driver = webdriver.Edge(service=serv, options=options)
+            else:
+                options = ChromeOptions()
+                options.add_argument("--headless")
+                options.add_argument("--disable-gpu")
+                options.add_argument("--window-size=1920,1080")
+                options.add_argument("--no-sandbox")
+                options.add_argument("--disable-dev-shm-usage")
+                options.add_argument("--disable-extensions")
+                options.add_argument("--disable-infobars")
+                serv = ChromeService(ChromeDriverManager().install())
+                driver = webdriver.Chrome(service=serv, options=options)
         else:
-            chrome_driver_path = os.path.join(project_folder, 'Resources', 'chromedriver')
-            serv = ChromeService(chrome_driver_path)
-            driver = webdriver.Chrome(service=serv)
+            if browser == "Edge":
+                edge_driver_path = os.path.join(project_folder, 'Resources', 'msedgedriver')
+                serv = EdgeService(edge_driver_path)
+                driver = webdriver.Edge(service=serv)
+            else:
+                chrome_driver_path = os.path.join(project_folder, 'Resources', 'chromedriver')
+                serv = ChromeService(chrome_driver_path)
+                driver = webdriver.Chrome(service=serv)
         driver.implicitly_wait(10)
         driver.maximize_window()
         yield driver
@@ -76,4 +105,3 @@ class BaseClass:
             subprocess.run(command, shell=True, check=True)
 
         request.addfinalizer(generate_report)'''
-
